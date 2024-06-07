ca44f21a9af969b34c128a2750b891bb1793d75c
José Quintino <josequintinno@icloud.com>
2024-06-06 22:29:27 -0300

    :small_blue_diamond: feature: Implementar persistencia dos acessos

--- resources/script/chave-seguranca-controller.js
+++ resources/script/chave-seguranca-controller.js
@@ -1,5 +1,6 @@
 const URL_CATEGORIA = "http://localhost:5500/resources/database/categoria.json";
 const URL_PESSOA_MONITORADA = "http://localhost:5500/resources/database/pessoa-monitorada.json";
+const URL_API_ACESSO = "http://localhost:8080/api/v1/acesso/";
 
 let categoriaList = [];
 let pessoaMonitoradaList = [];
@@ -70,6 +71,38 @@ function popularDadosPessoaMonitorada(pessoaMonitoradaList) {
             option.textContent = pessoa.nome;
         pessoaMonitoradaInput.appendChild(option);
     });
+
+}
+
+function persistir() {
+
+    event.preventDefault();
+
+    const codigoCategoria = document.getElementById("categoria-chave-acesso");
+    const codigoPessoaMonitorada = document.getElementById("pessoa-monitorada");
+    const identificador = document.getElementById("identificador");
+    const chave = document.getElementById("chave");
+    const aplicativo = document.getElementById("aplicativo");
+    const url = document.getElementById("url");
+
+    const chaveSegurancaObject = {
+        codigoCategoria: codigoCategoria.value,
+        codigoPessoaMonitorada: codigoPessoaMonitorada.value,
+        identificador: identificador.value,
+        chave: chave.value,
+        aplicativo: aplicativo != null ? aplicativo.value : null,
+        url: url != null ? url.value : null
+    }
+
+    fetch(URL_API_ACESSO, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(chaveSegurancaObject)
+    })
+    .then(response => response.ok ? console.log('Dados enviados com sucesso!') : Promise.reject('Erro ao enviar os dados. Status code: ' + response.status + '. Resposta da API: ' + response.statusText))
+    .catch(error => console.error('Erro de conexão ao enviar os dados para a API:', error));
 
 }
 
